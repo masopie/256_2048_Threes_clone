c881cacd0bfb131a0a4d243ba1f4b81948ad680f
Mason Pierce <masopie@gmail.com>
2016-04-26 21:11:37 -0500

    Get stupid jQuery error to go away

--- jasmine-standalone-2.4.1/spec/GameSpec.js
+++ jasmine-standalone-2.4.1/spec/GameSpec.js
@@ -1,8 +1,8 @@
 describe("Do the Jasmine tests even work", function(){
   it("works", function(){
     expect(true).toEqual(true);
-  })
-})
+  });
+});
 
 // OK whew
 
@@ -10,5 +10,10 @@ describe("Game", function(){
   it("exists", function(){
     var game = new Game("0020200000000000");
     expect(game).toEqual(jasmine.any(Game));
+  });
+
+  it("has a default board string", function(){
+    var game = new Game();
+    expect(game.board_string).toEqual("0020200000000000")
   })
-})
+});
