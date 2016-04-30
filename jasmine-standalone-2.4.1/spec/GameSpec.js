a8b2f46df3926dd8ec0a059cbd8938691fa7009d
Mason Pierce <masopie@gmail.com>
2016-04-30 18:20:03 -0500

    Testing

--- jasmine-standalone-2.4.1/spec/GameSpec.js
+++ jasmine-standalone-2.4.1/spec/GameSpec.js
@@ -7,6 +7,15 @@ describe("Do the Jasmine tests even work", function(){
 // OK whew
 
 describe("Game", function(){
+  var game;
+  var gameWithBoard;
+  var gameWithComplexBoard;
+
+  beforeEach(function(){
+    game = new Game();
+    gameWithBoard = new Game("0002000002000000")
+  })
+
   it("exists", function(){
     var game = new Game("0020200000000000");
     expect(game).toEqual(jasmine.any(Game));
@@ -22,7 +31,7 @@ describe("Game", function(){
     expect(game.board_string.length).toEqual(16)
   });
 
-  it("", function(){
-    
-  })
+  // it("turns its board string argument into a specified board", function(){
+  //   var
+  // })
 });
